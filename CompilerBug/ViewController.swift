db8ad6c54fc585fbdd54450067644dbd37150268
Tiziano Coroneo <coroneo@d-tt.nl>
2019-07-01 12:52:58 +0200

    Initial Commit

---
+++ CompilerBug/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CompilerBug
+//
+//  Created by Tiziano Coroneo on 01/07/2019.
+//  Copyright © 2019 Tiziano Coroneo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
